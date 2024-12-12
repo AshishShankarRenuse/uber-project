e953fd92a61eaf0c9ad823f8b77100ce5dd63dff
AshishShankarRenuse <ashishrenuse29@gmail.com>
2024-12-12 23:27:18 +0530

    captain login profile logout done

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -1,7 +1,9 @@
 const captainModel = require('../models/captain.model');
 const captainService = require('../services/captain.service');
-const {validationResult} = require('express-validator');
 const bcrypt = require('bcrypt');
+const blacklistTokenModel = require('../models/blacklistToken.model');
+const {validationResult} = require('express-validator');
+
 
 module.exports.registerCaptain = async (req, res,next) => {
     const errors = validationResult(req);
@@ -30,3 +32,35 @@ module.exports.registerCaptain = async (req, res,next) => {
     const token = captain.generateAuthToken();
     res.status(201).json({captain, token});
 }
+
+module.exports.loginCaptain = async (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {email, password} = req.body;
+    const captain = await captainModel.findOne({email}).select('+password');
+    if(!captain){
+        return res.status(400).json({message: 'Invalid captain email or password'});
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, captain.password);
+    if(!isPasswordCorrect){
+        return res.status(400).json({message: 'Invalid captain email or password'});
+    }
+    const token = captain.generateAuthToken();
+    res.cookie('token', token);
+    res.status(200).json({captain, token});
+} 
+
+module.exports.getCaptainProfile = async (req, res, next) => {
+    res.status(200).json({captain: req.captain});
+}
+
+module.exports.logoutCaptain = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+    await blacklistTokenModel.create({token});
+    res.clearCookie('token');
+    res.status(200).json({message: 'Captain Logged out successfully'});
+}
+
